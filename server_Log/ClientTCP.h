4cf54daabc5ee03986ed736769f56ddb59a7ad41
DmitryNazarov212 <dima_naz2002@mail.ru>
2025-04-02 20:49:10 +0300

    Логирование сообщений

---
+++ server_Log/ClientTCP.h
@@ -0,0 +1,22 @@
+﻿#ifndef TCPCLIENT_H
+#define TCPCLIENT_H
+
+#include <string>
+#include "Logger.h" // Добавлен include для Logger
+
+class TCPClient {
+public:
+    TCPClient(const std::string& serverIp, int port);
+    bool connectToServer();
+    void sendMessage(const std::string& message);
+    void receiveMessage();
+    void run();
+
+private:
+    std::string _serverIp;
+    int _port;
+    int _socket;
+    Logger logger; // Добавлен логгер
+};
+
+#endif // TCPCLIENT_H
---
+++ server_Log/ServerTCP.h
@@ -0,0 +1,28 @@
+﻿#ifndef TCPSERVER_H
+#define TCPSERVER_H
+
+#include <string>
+#include <map>
+#include "Logger.h" // Добавлен include для Logger
+
+class TCPServer {
+public:
+    TCPServer(int port);
+    bool start();
+    void acceptClients();
+    void handleClient(int clientSocket);
+    void sendToAllClients(const std::string& message);
+    void stopServer();
+
+private:
+    void loadUsersFromFile();  // Декларация функции загрузки пользователей
+    void saveUsersToFile();  // Декларация функции сохранения пользователей
+
+    int _port;
+    int _serverSocket;
+    std::map<std::string, std::string> users;  // Хранение пользователей
+    std::map<int, std::string> clientUsernames;  // Хранение пользователей, подключенных к серверу
+    Logger logger; // Добавлен логгер
+};
+
+#endif // TCPSERVER_H
